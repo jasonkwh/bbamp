029c1063da8a0b48d9ef3044ac8c9141b9aec33b
Jason Huang <jason.huang@contactspace.com>
2018-05-08 14:24:04 +1000

    Dynamic playlist window position according to main window position

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ const playBtn = document.getElementById('playbutton')
 const playlistBtn = document.getElementById('playlistbutton')
 
 let win
+var playlistwiny
 
 document.onmouseup = (event) => {
     if(win!=null) {
-        win.setPosition(current_win.getPosition()[0],current_win.getPosition()[1]+75,false)
+        if(current_win.getPosition()[1]>(window.screen.height/2)) {
+            playlistwiny = current_win.getPosition()[1] - 200
+        } else {
+            playlistwiny = current_win.getPosition()[1] + 75
+        }
+        win.setPosition(current_win.getPosition()[0],playlistwiny,false)
     }
 }
 
@@ -20,6 +26,12 @@ playBtn.addEventListener('click', function(event) {
 
 playlistBtn.addEventListener('click', function(event) {
     let pos = current_win.getPosition()
+    console.log(pos[1])
+    if(pos[1]>(window.screen.height/2)) {
+        playlistwiny = pos[1] - 200
+    } else {
+        playlistwiny = pos[1] + 75
+    }
     const modalPath = path.join('file://',__dirname,'playlist.html')
     if(win==null) {
         win = new BrowserWindow({
@@ -28,11 +40,10 @@ playlistBtn.addEventListener('click', function(event) {
             frame:false,
             resizable:false,
             maximizable:false,
-            alwaysOnTop:true,
             backgroundColor:'#262626',
             titleBarStyle:'hidden',
             x:pos[0],
-            y:pos[1] + 75
+            y:playlistwiny
         })
         win.on('close',function() {
             win = null
