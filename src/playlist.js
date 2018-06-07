dd4166c459f596c0464a50abcfc22aa5a687699e
Jason Huang <jason.huang@contactspace.com>
2018-06-07 10:38:19 +1000

    Can play music now!

--- src/playlist.js
+++ src/playlist.js
@@ -5,6 +5,7 @@ const current_win = electron.remote.getCurrentWindow();
 const Sortable = require('sortablejs');
 const closeBtn = document.getElementById('closebutton');
 const addPlaylistBtn = document.getElementById('playlistbtn');
+const { exec } = require('child_process');
 const list = document.getElementById("playlisttable");
 Sortable.create(list, {
     animation: 300
@@ -22,6 +23,17 @@ ipc.on('addToPlaylist', function(event,arg) {
     document.getElementById('playlisttable').innerHTML += '<li id="' + arg.id + '" class="playlistitem" onclick="playthisfile(this)" title="Drag to change order"><input type="hidden" class="songlocation" value="' + arg.filelocation + '"><input type="hidden" class="songduration" value="' + arg.duration + '"><table style="width:305px"><tr><td class="playstatus" style="width:40px"><i class="fas fa-play"></i></td><td style="text-align:left;width:220px"><table><tr style="line-height:13px"><td class="playlistsongname">' + arg.songname + '</td></tr><tr style="line-height:13px"><td style="font-size:10px">' + arg.artist + '</td></tr></table></td><td style="width:40px">' + arg.duration.split('.')[0] + '</td></tr></table></li>';
 });
 
+let playprocess = null;
 function playthisfile(e) {
-    console.log("haha");
+    if(playprocess!=null) {
+        playprocess.kill();
+    }
+    playprocess = exec('binaries/ffplay ' + e.getElementsByClassName('songlocation')[0].value + ' -nodisp -hide_banner', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
 }
